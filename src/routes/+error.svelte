93c838e12723f8fccab673e560b1176131c42387
Peter Simon <1401202+loonkwil@users.noreply.github.com>
2023-04-15 11:16:09 +0200

    Add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { page } from '$app/stores';
+
+  const message = `${$page.status} ${$page.error?.message ?? 'Something went wrong'}`;
+</script>
+
+<h2>(._. )</h2>
+<p>{message}</p>
+
+<style>
+  h2 {
+    font-weight: normal;
+    font-size: 6rem;
+    margin-bottom: 0;
+    text-align: center;
+    white-space: nowrap;
+  }
+
+  p {
+    margin: 0;
+    text-align: center;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,3 +6,13 @@
 <main>
   <slot />
 </main>
+
+<style>
+  main {
+    display: grid;
+    grid-template-rows: 1fr;
+    justify-content: center;
+    padding: 0.5rem;
+    gap: 1.5rem;
+  }
+</style>
