d2cb46ce5a10bbd48e979319e2339a3c7e026818
Peter Simon <1401202+loonkwil@users.noreply.github.com>
2023-07-01 10:46:21 +0200

    Update npm dependencies (Svelte v4)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,11 @@
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'prettier'],
-  plugins: ['svelte3'],
-  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:svelte/recommended',
+    'plugin:svelte/prettier'
+  ],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2020
